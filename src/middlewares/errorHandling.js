66308c9a07ddad3e37c97a6df33ac9e75c87659e
Victor <vihbuenoz@gmail.com>
2023-07-26 08:57:13 -0300

    adjust in a comment

--- src/middlewares/errorHandling.js
+++ src/middlewares/errorHandling.js
@@ -5,7 +5,7 @@ import notFound from '../errors/notFound.js'
 /* eslint-disable no-unused-vars */
 // Function to handle errors and send appropriate error responses
 function errorHandling(error, req, res, next) {
-    // Check if the error is an instance of 'notFound' error
+    // Check if the error is an instance of 'not found' error
     if (error instanceof notFound) {
         error.sendResponse(res)
 
